cebcda76670aa63362dee85ea8210f85e3caf94b
Mark Lotspaih <48569891+lotspaih@users.noreply.github.com>
2020-03-17 15:02:34 -0400

    Update chap1.ps1

--- chap1.ps1
+++ chap1.ps1
@@ -1,3 +1,15 @@
-Get-Command -Name
+Verb-Noun
 
-Get-Help
+Get-Command -Name*
+
+Command-Name [tab for completions]
+
+Get-Alias
+
+Get-Help CommandName -Examples
+Get-Help CommandName -Detailed
+Get-Help CommandName* - Full
+
+Get-Help - Name About*
+
+Update-Help
