01ee80053ba1070ee1456caafcc6104a4723ea38
Mark Lotspaih <48569891+lotspaih@users.noreply.github.com>
2020-03-26 14:54:43 -0400

    Create chap6.ps1

---
+++ chap6.ps1
@@ -0,0 +1,10 @@
+# WRITING FUNCTIONS
+
+Get-Command -CommandType Function
+
+function Install-MySoftware {
+    Write-Host 'Your software has been installed.'
+}
+
+# Verb-Noun naming convention
+Get-Verb
