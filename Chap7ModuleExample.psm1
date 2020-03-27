9db62684feb2063cbc4bf6951cba5c553633a796
Mark Lotspaih <48569891+lotspaih@users.noreply.github.com>
2020-03-27 12:00:18 -0400

    Add files via upload

---
+++ Chap7ModuleExample.psm1
@@ -0,0 +1,14 @@
+function Get-Software {
+    param()
+    # Gets software
+}
+
+function Install-Software {
+    param()
+    # Installs software
+}
+
+function Remove-Software {
+    param ()
+    # Removes software
+}
