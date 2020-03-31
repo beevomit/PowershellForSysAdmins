c02a32f575d4f90facd4dc36dff421ae80205a51
Mark Lotspaih <48569891+lotspaih@users.noreply.github.com>
2020-03-30 20:48:28 -0400

    Update chap8.ps1

--- chap8.ps1
+++ chap8.ps1
@@ -1,6 +1,6 @@
 # RUNNING SCRIPTS REMOTELY
 
-Enable-PSRemoting -Force
+Enable-PSRemoting -Force  # For systems older than Server 2012R2
 
 # ScriptBlock
 
@@ -9,4 +9,10 @@ Enable-PSRemoting -Force
 # Invoke-Command and New-PSSession
 
 Invoke-Command -ScriptBlock { hostname } -ComputerName WEBSRV1
+
+Invoke-Command -ComputerName WEBSRV1 -FilePath C:\GetHostName.ps1  # Invoke a script on a remote device
+
+Invoke-Command -ScriptBlock { Write-Host "The value of foo is $using:serverFilePath" } -ComputerName localhost  # Allows local variables to run on remote systems
+
+# PSSessions
 
