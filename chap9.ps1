873c7c769c3de993b4e91f4844559fd9520b6a85
Mark Lotspaih <48569891+lotspaih@users.noreply.github.com>
2020-04-01 13:41:13 -0400

    Create chap9.ps1

---
+++ chap9.ps1
@@ -0,0 +1,11 @@
+# TESTING WITH PESTER
+
+# extension .Tests.ps1
+
+#Format Structure
+describe
+    context
+        it
+            assertions
+            
+# See Sample.Tests.ps1 file for example
